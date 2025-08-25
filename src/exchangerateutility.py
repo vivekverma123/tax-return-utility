29916cfc254725d5c60c3830b2a38906148ae84b
Vivek Kumar Verma <123vivekverma@gmail.com>
2025-08-25 14:44:44 +0530

    Record the date for which stock price and exchange rate is being considered

--- src/exchangerateutility.py
+++ src/exchangerateutility.py
@@ -12,17 +12,18 @@ class ExchangeRateUtility:
         self.date_to_rate = {}
         self.lower_limit = datetime.strptime("2020-01-04", "%Y-%m-%d")
         self._initialize()
-        
+
     def _initialize(self):
         # DATE PDF_FILE TT_BUY TT_SELL BILL_BUY	BILL_SELL FOREX_TRAVEL_CARD BUY_FOREX_TRAVEL CARD_SELL CN_BUY CN_SELL
-        # For inward remittance TT_BUY is taken into account as the bank will buy foreign currency from you at that 
+        # For inward remittance TT_BUY is taken into account as the bank will buy foreign currency from you at that
         # exchange rate
+        # Check page 5 Enhancing Tax Transparency on Foreign Assets and Income.pdf
         df = pd.read_csv(self.path)
         for row in df.itertuples():
             if row is None:
                 continue
             self.date_to_rate[row.DATE.split(" ")[0]] = row._3
-            
+
     def _traverse(self, check, update, temp):
         rate = None
         while(rate is None and check(temp)):
@@ -34,23 +35,23 @@ class ExchangeRateUtility:
         return rate, temp_date
 
     def get_exchange_rate(self, date, cutOff=str(datetime.now().date())):
-        rate = None
-        dateStamp = datetime.strptime(date, "%Y-%m-%d")
-        temp = dateStamp
-        if temp in self.date_to_rate:
-            return self.date_to_rate[temp_date], temp
+        if date in self.date_to_rate:
+            return self.date_to_rate[date], date
 #        rate, temp_date = self._traverse(lambda temp: temp <= cutOff, lambda temp: temp + timedelta(days=1), temp)
 #        # No published exchange rate data upto the cutOff, look for the last available data prior to the date requested
 #        if rate is not None:
 #            return rate, temp_date
+        rate = None
+        dateStamp = datetime.strptime(date, "%Y-%m-%d")
+        temp = dateStamp
 
         cutOff = datetime.strptime(cutOff, "%Y-%m-%d")
         temp = dateStamp - timedelta(days=1)
-        rate, temp_date = self._traverse(lambda temp: temp >= self.lower_limit, 
+        rate, temp_date = self._traverse(lambda temp: temp >= self.lower_limit,
                                             lambda temp: temp - timedelta(days=1), temp)
-        
+
         if rate is None:
             assert 0, f"Data not available for the requested date {date}"
 
-        return rate, temp_date
+        return (rate, temp_date)
 
