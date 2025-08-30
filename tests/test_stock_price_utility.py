78b76e72aad3582845af1a01173d9d6666b15732
Vivek Kumar Verma <123vivekverma@gmail.com>
2025-08-30 21:16:53 +0530

    Rename testcases to exclude module name

--- tests/test_stock_price_utility.py
+++ tests/test_stock_price_utility.py
@@ -21,7 +21,7 @@ def retrieve_queried_collection(dates, start_date):
         idx += 1
     return dates[idx:]
 
-def test_stock_price_utility_all_days(test_case_data): # pylint: disable=W0621
+def test_all_days(test_case_data): # pylint: disable=W0621
     trading_days = test_case_data
     exchg_rt_utl = ExchangeRateUtility()
     stockpriceutil = StockPriceUtility("MSFT", "2023-01-01", "2023-12-31", exchg_rt_utl)
@@ -30,7 +30,7 @@ def test_stock_price_utility_all_days(test_case_data): # pylint: disable=W0621
     trading_days_retrieved = retrieve_queried_collection(trading_days_retrieved, '2023-01-01')
     assert trading_days_retrieved == trading_days
 
-def test_stock_price_utility_inclusivity(test_case_data): # # pylint: disable=W0621
+def test__inclusivity(test_case_data): # # pylint: disable=W0621
     exchg_rt_utl = ExchangeRateUtility()
     stockpriceutil = StockPriceUtility("MSFT", test_case_data[0], test_case_data[-1], exchg_rt_utl)
     assert test_case_data[-1] in stockpriceutil.date_to_peak_price
